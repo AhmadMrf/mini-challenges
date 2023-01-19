e73ce40790a2a49433c7114b7fcc9ae3be672e9f
naashna20@gmail.com <naashna20@gmail.com>
2023-01-19 16:13:12 +0330

    fix moving clock hand

--- Analog clock/index.js
+++ Analog clock/index.js
@@ -4,12 +4,11 @@ const sec = document.querySelector('.sec')
 
 const tick = ()=>{
   const now = new Date()
-  now.setHours(3,30,0)
   const second = now.getSeconds()
   const minutes = now.getMinutes()
   const hours = now.getHours()
   hr.style = `transform:rotateZ(${(hours*30+ minutes/2)}deg)`
-  min.style = `transform:rotateZ(${minutes*6}deg)`
+  min.style = `transform:rotateZ(${(minutes*6+ second/10)}deg)`
   sec.style = `transform:rotateZ(${second*6}deg)`
 }
 setInterval(tick,1000)
